5435f6ccb1bee2b5b0527ab402862028360d8ab3
Keith Redmond <keithredmond@gmail.com>
2020-03-03 17:49:36 +0000

    Added query for all Entities

--- Controllers/EntityController.cs
+++ Controllers/EntityController.cs
@@ -1,16 +1,38 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using MongoDAL;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace QueryApp.Controllers
 {
     [Route("api/[controller]")]
     public class EntityController : Controller
     {
+        MongoDBRepository mongoRepository = new MongoDBRepository("mongodb+srv://projector:projector@cluster0-gr1bz.mongodb.net/test?retryWrites=true&w=majority");
+
+        public EntityController()
+        {
+            mongoRepository.Connect();
+        }
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            IAsyncCursor<BsonDocument> cursor = mongoRepository.GetCollection("Entity").FindSync<BsonDocument>(FilterDefinition<BsonDocument>.Empty);
+            List<string> returnVal = new List<string>();
+            
+            while (cursor.MoveNext())
+            {
+                IEnumerable<BsonDocument> batch = cursor.Current;
+                foreach (BsonDocument document in batch)
+                {
+                    returnVal.Add(document.ToString());
+                }
+            }
+
+            return returnVal;
         }
 
         // GET api/values/5
